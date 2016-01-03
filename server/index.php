5f62f2b733b6e0a8c6c990fa918d405a7d80209f
Branislav Blaskovic <branislav@blaskovic.sk>
2016-01-03 18:20:00 +0100

    Just to make sure

--- server/index.php
+++ server/index.php
@@ -1,10 +1,12 @@
 <?php
 
-if(!file_exists('../restaurants/'.$_GET['menu']))
+$menu = preg_replace("/[^0-9\-\w]+/", "", $_GET['menu']);
+
+if(!file_exists('../restaurants/'.$menu))
 {
-    echo "Menu " . $_GET['menu'] . " does not exist. Use 'menu list' for the list or submit a patch: https://github.com/blaskovic/hungry-bot";
+    echo "Menu " . $menu . " does not exist. Use 'menu list' for the list or submit a patch: https://github.com/blaskovic/hungry-bot";
 }
 else
 {
-    echo exec("../restaurants/".$_GET['menu']);
+    echo exec("../restaurants/".$menu);
 }
